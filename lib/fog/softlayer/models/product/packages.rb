75fac4a36ea28aeb4c0a9f6c9bb92d74233430da
Celso Fernandes <fernandes@zertico.com>
2015-01-23 21:13:08 -0200

    Add initial support for ordering SoftLayer Products
    On SoftLayer to support products ordering we need to call
    Softlayer_Product_Order#placeOrder passing the right product container to this
    method.
    
    How we get the right container for an example a Load Balancer? We call
    Softlayer_Package#getAllObjects so we iterate over package (on our example
    case we choose the first one `Additional Products` then we call getITems.
    
    After getting all items for our package, we iterate to choose a load
    balancer one, like id __4126__
    `LOAD_BALANCER_500_VIP_CONNECTIONS_WITH_SSL_OFFLOAD` then we can place
    our order.
    
    In this commit we add models to enable this process and support future order
    implementations.

---
+++ lib/fog/softlayer/models/product/packages.rb
@@ -0,0 +1,29 @@
+#
+# Author:: Celso Fernandes (<fernandes@zertico.com>)
+# © Copyright IBM Corporation 2015.
+#
+# LICENSE: MIT (http://opensource.org/licenses/MIT)
+#
+require 'fog/core/collection'
+# require 'fog/softlayer/models/dns/records'
+
+module Fog
+  module Softlayer
+    class Product
+
+      class Packages < Fog::Collection
+
+        model Fog::Softlayer::Product::Package
+        
+        def initialize(attributes = {})
+          super(attributes)
+        end
+        
+        def all
+          data = service.get_packages.body
+          load(data)
+        end
+      end
+    end
+  end
+end
